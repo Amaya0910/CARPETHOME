350a86422c2a89692446768ba7490e4802fc72c7
Amaya0910 <iamayaf@unicesar.edu.co>
2025-10-27 13:53:59 -0500

    PRE-COMMIT

---
+++ src/main/java/grupo/proyecto_aula_carpethome/Launcher.java
@@ -0,0 +1,9 @@
+package grupo.proyecto_aula_carpethome;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
